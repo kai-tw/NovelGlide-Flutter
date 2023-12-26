a57e844892cb2981b25073faf5d4bb79bb9c8497
Kai <wuvincentck505@gmail.com>
2023-12-27 00:27:09 +0800

    Add path_provider dependency

--- lib/ui/pages/main/library.dart
+++ lib/ui/pages/main/library.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:novelglide/core/file_process.dart';
 
-class LibraryWidget extends StatelessWidget {
+class LibraryWidget extends StatefulWidget {
   const LibraryWidget({super.key});
 
   @override
+  State<LibraryWidget> createState() => _LibraryWidgetState();
+}
+class _LibraryWidgetState extends State<LibraryWidget> {
+  String path = "";
+
+  @override
+  void initState() {
+    super.initState();
+    FileProcess.get().then((value) {
+      setState(() {
+        path = value;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
         shadowColor: Colors.transparent,
         child: Center(
-            child: Text(
-                "Library"
-            )
+            child: Text(path)
         )
     );
   }
