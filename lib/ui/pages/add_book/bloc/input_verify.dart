2b4728cff6263f6d249577df595184d79bb819c7
Kai <wuvincentck505@gmail.com>
2023-12-29 18:01:59 +0800

    Build up the add book form
    + More translation

---
+++ lib/ui/pages/add_book/bloc/input_verify.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class InputVerifyCubit extends Cubit<InputVerifyState> {
+  InputVerifyCubit() : super(InputVerifyState());
+
+}
+
+class InputVerifyState extends Equatable {
+
+  @override
+  List<Object?> get props => [];
+}
--- lib/ui/pages/add_book/layout/scaffold.dart
+++ lib/ui/pages/add_book/layout/scaffold.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class AddBookPage extends StatelessWidget {
+class AddBookPage extends StatefulWidget {
   const AddBookPage({super.key});
 
+  @override
+  State<AddBookPage> createState() => _AddBookPageState();
+}
+
+class _AddBookPageState extends State<AddBookPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: _appBar(context), body: _bodyWidget());
@@ -11,7 +16,7 @@ class AddBookPage extends StatelessWidget {
 
   PreferredSizeWidget _appBar(BuildContext context) {
     return AppBar(
-      backgroundColor: Theme.of(context).colorScheme.background,
+        backgroundColor: Theme.of(context).colorScheme.background,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios_new_rounded),
           onPressed: () {
@@ -24,6 +29,37 @@ class AddBookPage extends StatelessWidget {
   }
 
   Widget _bodyWidget() {
-    return Container();
+    return SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              children: [
+                TextFormField(
+                    decoration: InputDecoration(
+                        labelText:
+                            AppLocalizations.of(context)!.add_book_book_name,
+                        labelStyle: const TextStyle(fontSize: 16),
+                        contentPadding: const EdgeInsets.all(24.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)))),
+                const SizedBox(height: 24),
+                SizedBox(
+                    width: double.infinity,
+                    height: 48,
+                    child: ElevatedButton(
+                        onPressed: _submitButtonPressed,
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor:
+                                Theme.of(context).colorScheme.onPrimary,
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary),
+                        child: Text(AppLocalizations.of(context)!.form_submit,
+                            style: TextStyle(
+                                color:
+                                    Theme.of(context).colorScheme.onPrimary))))
+              ],
+            )));
   }
+
+  void _submitButtonPressed() {}
 }
