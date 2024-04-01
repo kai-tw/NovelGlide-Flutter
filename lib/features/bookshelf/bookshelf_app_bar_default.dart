7475855e59fd17b1d7292792864a3884004d81f9
Kai <wuvincentck505@gmail.com>
2024-04-01 10:51:14 +0800

    [Bookshelf] Change title.

--- lib/features/bookshelf/bookshelf_app_bar_default.dart
+++ lib/features/bookshelf/bookshelf_app_bar_default.dart
@@ -14,7 +14,7 @@ class BookshelfAppBarDefault extends StatelessWidget implements PreferredSizeWid
         onPressed: () {},
         icon: const Icon(Icons.person),
       ),
-      title: Text(AppLocalizations.of(context)!.appName),
+      title: Text(AppLocalizations.of(context)!.titleBookshelf),
       centerTitle: false,
       backgroundColor: Theme.of(context).colorScheme.background,
       surfaceTintColor: Theme.of(context).colorScheme.background,
