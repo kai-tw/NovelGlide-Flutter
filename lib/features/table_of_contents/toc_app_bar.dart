c6c6e444879e5dd8739e5da20a7116339a32c8e9
Kai <wuvincentck505@gmail.com>
2024-04-01 23:08:51 +0800

    [TOC] Add title

--- lib/features/table_of_contents/toc_app_bar.dart
+++ lib/features/table_of_contents/toc_app_bar.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:novelglide/main.dart';
 
-import '../add_chapter/add_chapter_callee_add_button.dart';
 import '../common_components/common_back_button.dart';
 import '../edit_book/edit_book_callee_edit_button.dart';
 import 'bloc/toc_bloc.dart';
@@ -14,11 +15,13 @@ class TOCAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    TOCCubit cubit = BlocProvider.of<TOCCubit>(context);
+    final TOCCubit cubit = BlocProvider.of<TOCCubit>(context);
+    final AppLocalizations appLocalizations = AppLocalizations.of(context)!;
     return AppBar(
       backgroundColor: Theme.of(context).colorScheme.background,
       surfaceTintColor: Theme.of(context).colorScheme.background,
       leading: CommonBackButton(popValue: cubit.isDirty),
+      title: Text(appLocalizations.titleTOC),
       actions: [
         EditBookCalleeEditButton(
           bookObject: cubit.bookObject,
