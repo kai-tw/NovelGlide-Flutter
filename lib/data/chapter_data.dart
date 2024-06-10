827544c92a53b7bd8bc07c601295dd6ffe320a7e
Kai <wuvincentck505@gmail.com>
2024-06-10 22:16:25 +0800

    Optimization
    - Add comments
    - Processor processes the data

--- lib/data/chapter_data.dart
+++ lib/data/chapter_data.dart
@@ -1,7 +1,4 @@
 import 'dart:io';
-import 'dart:typed_data';
-
-import 'package:flutter_charset_detector/flutter_charset_detector.dart';
 
 import '../toolbox/chapter_processor.dart';
 
@@ -39,34 +36,10 @@ class ChapterData {
   }
 
   Future<bool> create(File file, {String? title}) async {
-    Uint8List bytes = await file.readAsBytes();
-    DecodingResult result = await CharsetDetector.autoDecode(bytes);
-    List<String> contentLines = result.string.split("\n").where((line) => line.isNotEmpty).toList();
-
-    // Create chapter file
-    File chapterFile = File(getPath());
-    await chapterFile.create();
-
-    // Prepend title
-    if (title != null) {
-      contentLines.insert(0, title);
-    }
-
-    // Write to file
-    await chapterFile.writeAsString(contentLines.join("\n"));
-
-    return chapterFile.existsSync();
+    return await ChapterProcessor.create(bookName, ordinalNumber, file, title: title);
   }
 
   Future<bool> delete() async {
-    final File file = File(getPath());
-
-    if (!file.existsSync()) {
-      return false;
-    }
-
-    await file.delete();
-
-    return !file.existsSync();
+    return await ChapterProcessor.delete(bookName, ordinalNumber);
   }
 }
--- lib/toolbox/chapter_processor.dart
+++ lib/toolbox/chapter_processor.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:collection/collection.dart';
+import 'package:flutter_charset_detector/flutter_charset_detector.dart';
 import 'package:mime/mime.dart';
 import 'package:path/path.dart';
 
@@ -13,6 +15,7 @@ import 'book_processor.dart';
 class ChapterProcessor {
   static final RegExp _chapterRegexp = RegExp(r'^\d+\.txt$');
 
+  /// Get all the chapter data of a book.
   static List<ChapterData> getList(String bookName) {
     final Directory folder = Directory(BookProcessor.getPathByName(bookName));
 
@@ -35,10 +38,12 @@ class ChapterProcessor {
     return [];
   }
 
+  /// Get the path of a chapter.
   static String getPath(String bookName, int ordinalNumber) {
     return join(BookProcessor.getPathByName(bookName), "$ordinalNumber.txt");
   }
 
+  /// Get the content of a chapter.
   static Future<List<String>> getContent(String bookName, int ordinalNumber) async {
     final File file = File(getPath(bookName, ordinalNumber));
     if (!file.existsSync()) {
@@ -58,13 +63,14 @@ class ChapterProcessor {
     return contentLines;
   }
 
+  /// Get the title of a chapter.
   static Future<String> getTitle(String bookName, int ordinalNumber) async {
     final File file = File(getPath(bookName, ordinalNumber));
     if (file.existsSync()) {
       final Stream<List<int>> inputStream = file.openRead();
       final Stream<String> lineStream = inputStream.transform(utf8.decoder).transform(const LineSplitter());
       await for (String line in lineStream) {
-        // Get the first not empty line as title.
+        // Get the first non-empty line as title.
         String singleLine = line.trim();
         if (singleLine.isNotEmpty) {
           return singleLine;
@@ -72,5 +78,36 @@ class ChapterProcessor {
       }
     }
     return "";
+  }
+
+  /// Create a chapter.
+  static Future<bool> create(String bookName, int ordinalNumber, File file, {String? title}) async {
+    Uint8List bytes = await file.readAsBytes();
+    DecodingResult result = await CharsetDetector.autoDecode(bytes);
+    List<String> contentLines = result.string.split(Platform.lineTerminator).where((line) => line.isNotEmpty).toList();
+
+    // Create chapter file
+    File chapterFile = File(getPath(bookName, ordinalNumber));
+    chapterFile.createSync();
+
+    // Prepend title
+    if (title != null) {
+      contentLines.insert(0, title);
+    }
+
+    // Write to file
+    chapterFile.writeAsStringSync(contentLines.join(Platform.lineTerminator));
+
+    return chapterFile.existsSync();
+  }
+
+  /// Delete a chapter.
+  static Future<bool> delete(String bookName, int ordinalNumber) async {
+    final File file = File(getPath(bookName, ordinalNumber));
+    if (file.existsSync()) {
+      file.deleteSync();
+      return !file.existsSync();
+    }
+    return false;
   }
 }
