37239d3fce2f1b8b5434045be355127a2a16a647
Kai <wuvincentck505@gmail.com>
2024-07-03 00:07:52 +0800

    Add iOS Ad id

--- lib/ad_center/advertisement_id.dart
+++ lib/ad_center/advertisement_id.dart
@@ -4,8 +4,8 @@ class AdvertisementId {
   static String get adaptiveBanner {
     if (Platform.isAndroid) {
       return 'ca-app-pub-1579558558142906/6034508731';
-    // } else if (Platform.isIOS) {
-    //   return '';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-1579558558142906/3980025184';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
