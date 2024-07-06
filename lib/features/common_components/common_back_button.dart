e81e35e647b88701f3cc83aaca80723427de4609
Kai <wuvincentck505@gmail.com>
2024-07-07 03:10:44 +0800

    Add back key semantic

--- lib/features/common_components/common_back_button.dart
+++ lib/features/common_components/common_back_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CommonBackButton extends StatelessWidget {
   const CommonBackButton({this.onPressed, super.key, this.popValue});
@@ -8,15 +9,21 @@ class CommonBackButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        Navigator.of(context).pop(popValue);
+    final AppLocalizations appLocalizations = AppLocalizations.of(context)!;
+    return Semantics(
+      label: appLocalizations.accessibilityBackButton,
+      button: true,
+      enabled: true,
+      child: IconButton(
+        onPressed: () {
+          Navigator.of(context).pop(popValue);
 
-        if (onPressed != null) {
-          onPressed!();
-        }
-      },
-      icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          if (onPressed != null) {
+            onPressed!();
+          }
+        },
+        icon: const Icon(Icons.arrow_back_ios_new_rounded),
+      ),
     );
   }
 }
