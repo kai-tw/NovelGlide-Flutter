c894552e1751c882ee636ecd42754018b24e4011
Kai <wuvincentck505@gmail.com>
2024-07-08 00:51:40 +0800

    Sync layout to editing page

--- lib/features/edit_book/edit_book_scaffold.dart
+++ lib/features/edit_book/edit_book_scaffold.dart
@@ -1,9 +1,9 @@
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../data/book_data.dart';
+import '../../data/window_class.dart';
 import '../common_components/common_back_button.dart';
 import 'bloc/edit_book_form_bloc.dart';
 import 'widgets/edit_book_image_picker.dart';
@@ -17,34 +17,91 @@ class EditBookScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ThemeSwitchingArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: const CommonBackButton(),
-          title: Text(AppLocalizations.of(context)!.titleEditBook),
-        ),
-        body: BlocProvider(
-          create: (_) => EditBookFormCubit(bookData),
-          child: const Padding(
-            padding: EdgeInsets.all(24.0),
-            child: Form(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 32.0),
-                      child: EditBookNameInputField(),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 16.0),
-                      child: EditBookImagePicker(),
-                    ),
-                    EditBookSubmitButton(),
-                  ],
-                ),
-              ),
+    final WindowClass windowClass = WindowClassExtension.getClassByWidth(MediaQuery.of(context).size.width);
+    final Widget bodyWidget;
+
+    switch (windowClass) {
+      case WindowClass.compact:
+        bodyWidget = const EditBookScaffoldCompactView();
+        break;
+      default:
+        bodyWidget = const EditBookScaffoldMediumView();
+        break;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: const CommonBackButton(),
+        title: Text(AppLocalizations.of(context)!.titleEditBook),
+      ),
+      body: Form(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: BlocProvider(
+              create: (_) => EditBookFormCubit(bookData),
+              child: bodyWidget,
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class EditBookScaffoldCompactView extends StatelessWidget {
+  const EditBookScaffoldCompactView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(bottom: 32.0),
+          child: EditBookNameInputField(),
+        ),
+        Padding(
+          padding: EdgeInsets.only(bottom: 32.0),
+          child: EditBookImagePicker(),
+        ),
+        EditBookSubmitButton(),
+      ],
+    );
+  }
+}
+
+class EditBookScaffoldMediumView extends StatelessWidget {
+  const EditBookScaffoldMediumView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 600),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 32.0),
+              child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(right: 16.0),
+                        width: constraints.maxWidth / 2,
+                        child: const EditBookNameInputField(),
+                      ),
+                      const Expanded(
+                        child: EditBookImagePicker(),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            const EditBookSubmitButton(),
+          ],
         ),
       ),
     );
