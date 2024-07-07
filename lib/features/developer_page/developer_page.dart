7d25559d435c6d10ed9d8f3773a269e5b2f71ca1
Kai <wuvincentck505@gmail.com>
2024-07-07 22:26:06 +0800

    Add a test button
    Create a fake book with a clicking.

--- lib/features/developer_page/developer_page.dart
+++ lib/features/developer_page/developer_page.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
+import '../../data/book_data.dart';
+import '../../processor/chapter_processor.dart';
+import '../../toolbox/random_utility.dart';
 import '../common_components/common_back_button.dart';
 import '../settings_page/widgets/setting_page_button.dart';
 import 'device_info_panel.dart';
@@ -25,7 +31,37 @@ class DeveloperPage extends StatelessWidget {
             iconData: Icons.error_outline_rounded,
             label: 'Force crash',
           ),
+          SettingPageButton(
+            onPressed: () => _createFakeBook(context),
+            iconData: Icons.code_rounded,
+            label: 'Generate a fake book',
+          ),
         ],
+      ),
+    );
+  }
+
+  void _createFakeBook(BuildContext context) {
+    BookData book;
+    do {
+      final String randomString = RandomUtility.getRandomString(8);
+      final String bookName = 'FakeBook_$randomString';
+      book = BookData(name: bookName);
+    } while (book.isExists());
+    book.create();
+
+    final int chapterCount = Random().nextInt(20);
+    for (int i = 0; i < chapterCount; i++) {
+      final int ordinalNumber = Random().nextInt(100) + 1;
+      final List<String> contentList =
+          List.generate(Random().nextInt(100) + 1, (_) => RandomUtility.getRandomString(100));
+      File chapterFile = File(ChapterProcessor.getPath(book.name, ordinalNumber));
+      chapterFile.writeAsStringSync(contentList.join(Platform.lineTerminator));
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Fake book ${book.name} created, and $chapterCount chapters created.'),
       ),
     );
   }
