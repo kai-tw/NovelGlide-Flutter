9e741ba409a1bbeb59eac32e2b6ff45e77949278
Kai <wuvincentck505@gmail.com>
2024-07-09 22:43:50 +0800

    Remove unused code

--- lib/features/bookshelf/bloc/bookshelf_bloc.dart
+++ lib/features/bookshelf/bloc/bookshelf_bloc.dart
@@ -14,63 +14,28 @@ class BookshelfCubit extends Cubit<BookshelfState> {
     BookshelfStateCode code = list.isEmpty ? BookshelfStateCode.empty : BookshelfStateCode.normal;
     emit(BookshelfState(code: code, bookList: list));
   }
-
-  void addSelect(String name) {
-    Set<String> selectedSet = Set<String>.from(state.selectedSet);
-    selectedSet.add(name);
-    emit(state.copyWith(code: BookshelfStateCode.selecting, selectedSet: selectedSet));
-  }
-
-  void removeSelect(String name) {
-    Set<String> selectedSet = Set<String>.from(state.selectedSet);
-    selectedSet.remove(name);
-    emit(state.copyWith(
-      code: selectedSet.isEmpty ? BookshelfStateCode.normal : BookshelfStateCode.selecting,
-      selectedSet: selectedSet,
-    ));
-  }
-
-  void allSelect() {
-    final Set<String> selectedSet = state.bookList.map((book) => book.name).toSet();
-    emit(state.copyWith(code: BookshelfStateCode.selecting, selectedSet: selectedSet));
-  }
-
-  void clearSelect() {
-    emit(state.copyWith(code: BookshelfStateCode.normal, selectedSet: const {}));
-  }
-
-  void deleteSelect() {
-    for (String bookName in state.selectedSet) {
-      BookData.fromName(bookName).delete();
-    }
-    refresh();
-  }
 }
 
 class BookshelfState extends Equatable {
   final BookshelfStateCode code;
   final List<BookData> bookList;
-  final Set<String> selectedSet;
 
   @override
-  List<Object?> get props => [code, bookList, selectedSet];
+  List<Object?> get props => [code, bookList];
 
   const BookshelfState({
     this.code = BookshelfStateCode.loading,
     this.bookList = const [],
-    this.selectedSet = const {},
   });
 
   BookshelfState copyWith({
     BookshelfStateCode? code,
     List<BookData>? bookList,
-    Set<String>? selectedSet,
     bool? refreshTrigger,
   }) {
     return BookshelfState(
       code: code ?? this.code,
       bookList: bookList ?? this.bookList,
-      selectedSet: selectedSet ?? this.selectedSet,
     );
   }
 }
