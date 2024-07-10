6868e0cd169bee09e48dfb6387aba355cfe4a48e
Kai <wuvincentck505@gmail.com>
2024-07-10 18:00:22 +0800

    Show add book form in a dialog in large screen

--- lib/features/bookshelf/widgets/bookshelf_add_book_button.dart
+++ lib/features/bookshelf/widgets/bookshelf_add_book_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../../../data/window_class.dart';
 import '../../add_book/add_book_scaffold.dart';
 import '../bloc/bookshelf_bloc.dart';
 
@@ -17,25 +18,45 @@ class BookshelfAddBookButton extends StatelessWidget {
       enabled: true,
       child: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (BuildContext context) => const AddBookScaffold()))
-              .then((isSuccess) {
-            final BookshelfCubit cubit = BlocProvider.of<BookshelfCubit>(context);
-            final AppLocalizations appLocalizations = AppLocalizations.of(context)!;
-            if (isSuccess == true) {
-              cubit.refresh();
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text(appLocalizations.addBookSuccessfully),
-              ));
-            } else if (isSuccess == false) {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text(appLocalizations.addBookFailed),
-              ));
-            }
-          });
+          final WindowClass windowClass = WindowClassExtension.getClassByWidth(MediaQuery.of(context).size.width);
+          switch (windowClass) {
+            case WindowClass.compact:
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (BuildContext context) => const AddBookScaffold()))
+                  .then((isSuccess) => _onBack(context, isSuccess));
+              break;
+            default:
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const Dialog(
+                    clipBehavior: Clip.hardEdge,
+                    child: SizedBox(
+                      width: 360.0,
+                      child: AddBookScaffold(),
+                    ),
+                  );
+                },
+              ).then((isSuccess) => _onBack(context, isSuccess));
+          }
         },
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void _onBack(BuildContext context, dynamic isSuccess) {
+    final BookshelfCubit cubit = BlocProvider.of<BookshelfCubit>(context);
+    final AppLocalizations appLocalizations = AppLocalizations.of(context)!;
+    if (isSuccess == true) {
+      cubit.refresh();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(appLocalizations.addBookSuccessfully),
+      ));
+    } else if (isSuccess == false) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(appLocalizations.addBookFailed),
+      ));
+    }
   }
 }
