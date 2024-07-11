6295bed0cde5a53c225a86996d631f7a7ae8e02b
Kai <wuvincentck505@gmail.com>
2024-07-11 23:29:04 +0800

    Semantic Improvement

--- lib/features/reader_settings/widgets/reader_settings_font_size_slider.dart
+++ lib/features/reader_settings/widgets/reader_settings_font_size_slider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../data/reader_settings_data.dart';
 import '../../reader/bloc/reader_cubit.dart';
@@ -20,15 +21,20 @@ class ReaderSettingsFontSizeSlider extends StatelessWidget {
             Icons.format_size_rounded,
             color: Theme.of(context).colorScheme.primary,
             size: ReaderSettingsData.minFontSize,
+            semanticLabel: AppLocalizations.of(context)!.accessibilityFontSizeSliderMinIcon,
           ),
           rightIcon: Icon(
             Icons.format_size_rounded,
             color: Theme.of(context).colorScheme.primary,
             size: ReaderSettingsData.maxFontSize,
+            semanticLabel: AppLocalizations.of(context)!.accessibilityFontSizeSliderMaxIcon,
           ),
           min: ReaderSettingsData.minFontSize,
           max: ReaderSettingsData.maxFontSize,
           value: state.fontSize,
+          semanticFormatterCallback: (double value) {
+            return '${AppLocalizations.of(context)!.accessibilityFontSizeSlider} ${value.toStringAsFixed(1)}';
+          },
           onChanged: (double value) {
             cubit.setState(fontSize: value);
             readerCubit.setSettings(fontSize: value);
