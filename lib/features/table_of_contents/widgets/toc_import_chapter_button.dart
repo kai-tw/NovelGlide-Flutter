420d1fb54fd5043dfeeb8dfd3c49b53cf1495d36
Kai <wuvincentck505@gmail.com>
2024-07-20 00:00:12 +0800

    Remove unused importation

--- lib/features/table_of_contents/widgets/toc_import_chapter_button.dart
+++ lib/features/table_of_contents/widgets/toc_import_chapter_button.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../data/book_data.dart';
-import '../../../data/window_class.dart';
 import '../../chapter_importer/chapter_importer_scaffold.dart';
 import '../bloc/toc_bloc.dart';
 
