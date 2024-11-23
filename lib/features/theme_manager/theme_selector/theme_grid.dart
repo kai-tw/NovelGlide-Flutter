695535b4dcb744d52ad1b496f6d29ee5426efd28
Kai <wuvincentck505@gmail.com>
2024-11-24 01:45:34 +0800

    Theme Grid Improvement

--- lib/features/theme_manager/theme_selector/theme_grid.dart
+++ lib/features/theme_manager/theme_selector/theme_grid.dart
@@ -4,43 +4,37 @@ class _ThemeGrid extends StatelessWidget {
   const _ThemeGrid();
 
   static const double _maxAxisExtent = 100.0;
-  static const double _maxCrossAxisExtent = 100.0;
+  static const double _maxCrossAxisExtent = 92.0;
 
   @override
   Widget build(BuildContext context) {
     final cubit = BlocProvider.of<ThemeManagerCubit>(context);
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final parentWidth = constraints.maxWidth;
-        final crossAxisCount = parentWidth ~/ _maxCrossAxisExtent;
-        final rowCount = (ThemeId.values.length / crossAxisCount).ceil();
-        return SizedBox(
-          height: min<double>(200, rowCount * _maxAxisExtent),
-          child: CustomScrollView(
-            controller: cubit.scrollController,
-            slivers: [
-              SliverPadding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                sliver: SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) => _ThemeSwitcher(
-                      themeId: ThemeId.values[index],
-                    ),
-                    childCount: ThemeId.values.length,
-                  ),
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    crossAxisSpacing: 8.0,
-                    mainAxisSpacing: 8.0,
-                    mainAxisExtent: _maxAxisExtent,
-                    maxCrossAxisExtent: _maxCrossAxisExtent,
-                  ),
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxHeight: 200.0),
+      child: CustomScrollView(
+        controller: cubit.scrollController,
+        shrinkWrap: true,
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            sliver: SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => _ThemeSwitcher(
+                  themeId: ThemeId.values[index],
                 ),
+                childCount: ThemeId.values.length,
               ),
-            ],
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+                mainAxisExtent: _maxAxisExtent,
+                maxCrossAxisExtent: _maxCrossAxisExtent,
+              ),
+            ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
