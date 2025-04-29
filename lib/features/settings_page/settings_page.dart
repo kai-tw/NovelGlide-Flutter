0d867750865218a3ad14886ae5cab5afaef0a2bd
Kai <wuvincentck505@gmail.com>
2025-04-29 10:06:07 +0800

    refactor: backupManager -> backupService

--- lib/features/settings_page/settings_page.dart
+++ lib/features/settings_page/settings_page.dart
@@ -6,7 +6,7 @@ import '../../enum/window_class.dart';
 import '../../generated/i18n/app_localizations.dart';
 import '../../utils/route_utils.dart';
 import '../about_page/about_page.dart';
-import '../backup_manager/backup_service_page.dart';
+import '../backup_service/backup_service_page.dart';
 import '../developer_page/developer_page.dart';
 import '../reset_page/reset_page.dart';
 import '../tts_service/tts_service.dart';
