1e960794dae0db98703c2aff8225f77a9ee5239d
Kai <wuvincentck505@gmail.com>
2025-07-03 01:43:14 +0800

    Disable debug printing

--- lib/features/reader/presentation/reader_page/cubit/reader_web_view_handler.dart
+++ lib/features/reader/presentation/reader_page/cubit/reader_web_view_handler.dart
@@ -44,7 +44,7 @@ class ReaderWebViewHandler {
     final Map<String, dynamic> data = jsonDecode(message.message);
     assert(data['route'] is String);
     _channelMap[data['route']]?.call(data['data']);
-    debugPrint("Receive: ${data['route']} ${data['data']}");
+    // debugPrint("Receive: ${data['route']} ${data['data']}");
   }
 
   void register(String route, void Function(dynamic) handler) {
