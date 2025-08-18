78b22eb49716ded556c985eb0393a419ed2afbd0
Kai <wuvincentck505@gmail.com>
2025-08-18 23:05:20 +0800

    Disable Crashlytics log errors in debug mode

--- lib/core/log_system/data/data_sources/adapters/firebase_crashlytics_adapter.dart
+++ lib/core/log_system/data/data_sources/adapters/firebase_crashlytics_adapter.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 
 import '../log_data_source.dart';
 
@@ -6,34 +7,52 @@ class FirebaseCrashlyticsAdapter extends LogDataSource {
   FirebaseCrashlyticsAdapter(this._instance);
 
   final FirebaseCrashlytics _instance;
+  final bool _enableLogging = kDebugMode;
 
   @override
-  Future<void> error(String message, {Object? error, StackTrace? stackTrace}) {
-    return _instance.recordError(
-      Exception(message),
-      stackTrace,
-      reason: message,
-      fatal: false,
-    );
+  Future<void> error(
+    String message, {
+    Object? error,
+    StackTrace? stackTrace,
+  }) async {
+    if (_enableLogging) {
+      return _instance.recordError(
+        error,
+        stackTrace,
+        reason: message,
+        fatal: false,
+      );
+    }
   }
 
   @override
-  Future<void> fatal(String message, {Object? error, StackTrace? stackTrace}) {
-    return _instance.recordError(
-      Exception(message),
-      stackTrace,
-      reason: message,
-      fatal: true,
-    );
+  Future<void> fatal(
+    String message, {
+    Object? error,
+    StackTrace? stackTrace,
+  }) async {
+    if (_enableLogging) {
+      return _instance.recordError(
+        error,
+        stackTrace,
+        reason: message,
+        fatal: true,
+      );
+    }
   }
 
   @override
-  Future<void> info(String message) {
-    return _instance.log('Info: $message');
+  Future<void> info(String message) async {
+    if (_enableLogging) {
+      return _instance.log('Info: $message');
+    }
   }
 
   @override
-  Future<void> event(String name, {Map<String, dynamic>? parameters}) async {
+  Future<void> event(
+    String name, {
+    Map<String, dynamic>? parameters,
+  }) async {
     // No-ops
   }
 }
